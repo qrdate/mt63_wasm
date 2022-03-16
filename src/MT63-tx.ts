339fdc8e62ebb25e05da8266bffeb653c05d01cf
Kris Slyka <kris@slyka.net>
2022-03-16 13:29:07 +0100

    Adopt qrdate style

--- src/MT63-tx.ts
+++ src/MT63-tx.ts
@@ -1,42 +1,42 @@
-import { TinyWASI } from "tinywasi";
+import { TinyWASI } from 'tinywasi';
 
-export class MT63Tx
-{
-	private tinyWASI: TinyWASI = new TinyWASI();
-	private instance: WebAssembly.Instance;
+export class MT63Tx {
+    private tinyWASI: TinyWASI = new TinyWASI();
+    private instance: WebAssembly.Instance;
 
-	constructor( source: BufferSource )
-	{
-		const mod = new WebAssembly.Module( source );
-		this.instance = new WebAssembly.Instance( mod, this.tinyWASI.imports );
-		this.tinyWASI.initialize( this.instance );
-	}
+    constructor(source: BufferSource) {
+        const mod = new WebAssembly.Module(source);
+        this.instance = new WebAssembly.Instance(mod, this.tinyWASI.imports);
+        this.tinyWASI.initialize(this.instance);
+    }
 
-	transmit( text: string, center: number, bandwidth: 500 | 1000 | 2000, interleave: 0 | 1 ): Float32Array
-	{
-		const getInputBuffer = this.instance.exports.getInputBuffer as CallableFunction;
-		const transmit = this.instance.exports.transmit as CallableFunction;
-		const getOutputBuffer = this.instance.exports.getOutputBuffer as CallableFunction;
+    transmit(
+        text: string,
+        center: number,
+        bandwidth: 500 | 1000 | 2000,
+        interleave: 0 | 1,
+    ): Float32Array {
+        const getInputBuffer = this.instance.exports.getInputBuffer as CallableFunction;
+        const transmit = this.instance.exports.transmit as CallableFunction;
+        const getOutputBuffer = this.instance.exports.getOutputBuffer as CallableFunction;
 
-		const data = new TextEncoder().encode( text );
+        const data = new TextEncoder().encode(text);
 
-		const textPtr = getInputBuffer( data.byteLength );
-		const memory = new Uint8Array( this.getMemory().buffer, textPtr, data.byteLength );
-		memory.set( data );
+        const textPtr = getInputBuffer(data.byteLength);
+        const memory = new Uint8Array(this.getMemory().buffer, textPtr, data.byteLength);
+        memory.set(data);
 
-		const audioLength = transmit( center, bandwidth, interleave );
+        const audioLength = transmit(center, bandwidth, interleave);
 
-		if( audioLength == -1 )
-			throw new Error( "Encoding Failed!" );
+        if (audioLength === -1) throw new Error('Encoding Failed!');
 
-		const audioPtr = getOutputBuffer();
-		const audio = new Float32Array( this.getMemory().buffer, audioPtr, audioLength );
+        const audioPtr = getOutputBuffer();
+        const audio = new Float32Array(this.getMemory().buffer, audioPtr, audioLength);
 
-		return audio.subarray();
-	}
+        return audio.subarray();
+    }
 
-	private getMemory(): WebAssembly.Memory
-	{
-		return ( this.instance.exports.memory as WebAssembly.Memory );
-	}
+    private getMemory(): WebAssembly.Memory {
+        return this.instance.exports.memory as WebAssembly.Memory;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { MT63Tx } from "./MT63-tx.js";
-export { source as mt63TxSource } from "./mt63-tx-source.js";
-export { source as mt63RxSource } from "./mt63-rx-source.js";
+export { MT63Tx } from './MT63-tx.js';
+export { source as mt63TxSource } from './mt63-tx-source.js';
+export { source as mt63RxSource } from './mt63-rx-source.js';
